48f4894b9a1f27832af5a9203f164efdcea06ca8
iamkaraan <randyk@zylun.com>
2017-04-25 09:19:45 +0800

    updates
    updates for front-end and back-end

--- my-pizza-store/functions.php
+++ my-pizza-store/functions.php
@@ -50,6 +50,8 @@ add_action( 'widgets_init', 'glasseye_widgets_init' );
 /*
 ---------  Custom Classes Post Type --------
 */
+
+add_theme_support('post-thumbnails'); /* this line will add Featured Image meta box in post type editor */
     
 function create_post_type() {
   register_post_type( 'glasseye_classes',
@@ -75,12 +77,53 @@ function create_post_type() {
       'rewrite' => true,
       'capability_type' => 'post',
       'hierarchical' => false,
-      'supports' => array('title','thumbnail')
+      'supports' => array('thumbnail','title')
     )
   );
     
 }
 add_action( 'init', 'create_post_type' );
+
+
+function awesome_custom_post_type (){
+	
+	$labels = array(
+		'name' => 'Portfolio',
+		'singular_name' => 'Portfolio',
+		'add_new' => 'Add Item',
+		'all_items' => 'All Items',
+		'add_new_item' => 'Add Item',
+		'edit_item' => 'Edit Item',
+		'new_item' => 'New Item',
+		'view_item' => 'View Item',
+		'search_item' => 'Search Portfolio',
+		'not_found' => 'No items found',
+		'not_found_in_trash' => 'No items found in trash',
+		'parent_item_colon' => 'Parent Item'
+	);
+	$args = array(
+		'labels' => $labels,
+		'public' => true,
+		'has_archive' => true,
+		'publicly_queryable' => true,
+		'query_var' => true,
+		'rewrite' => true,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'supports' => array(
+			'title',
+			'editor',
+			'excerpt',
+			'thumbnail',
+			'revisions',
+		),
+		'taxonomies' => array('category', 'post_tag'),
+		'menu_position' => 5,
+		'exclude_from_search' => false
+	);
+	register_post_type('portfolio',$args);
+}
+add_action('init','awesome_custom_post_type');
     
     // define prefix 
     $prefix = 'pz_';
@@ -107,7 +150,6 @@ add_action( 'init', 'create_post_type' );
                 'type' => 'text',
                 'std' => 'Leslie Yepp'
             ),
-            */
             array(
               'name' => 'Pizza package image',
               'descr' => 'Please paste the full URL to the image/photo. You can find this in the Media Libary',
@@ -115,6 +157,7 @@ add_action( 'init', 'create_post_type' );
               'type' => 'text',
               'std' => ''
             ),
+            */
             array(
                 'name' => 'Package Price',
                 'descr' => 'Php 200, Php 350',
--- my-pizza-store/page-classes.php
+++ my-pizza-store/page-classes.php
@@ -24,13 +24,17 @@
               // returns the key for the Name field for each Class
               $class_name_key = $meta_box_fields[0]['id'];
               $class_name_value = get_post_meta($post->ID, $class_name_key, true);
-              
+    
+              //echo $class_name_value.'<br>';              
+    
               // returns the key for the Instructor field
               $class_instructor_key = $meta_box_fields[1]['id'];
               $class_instructor_value = get_post_meta ($post->ID, $class_instructor_key, true);
+              //echo $class_instructor_value.'<br>';
               
               $class_instructor_thumbnail_key = $meta_box_fields[2]['id'];
               $class_instructor_thumbnail_value = get_post_meta ($post->ID, $class_instructor_thumbnail_key, true);
+              //echo $class_instructor_thumbnail_value.'<br>';
               
               $class_skill_key = $meta_box_fields[3]['id'];
               $class_skill_value = get_post_meta ($post->ID, $class_skill_key, true);
@@ -46,23 +50,43 @@
               
               ?>
           <div class="class-container class-container-<?php echo strtolower($class_theme_value) ?>">    
-              <h2><?php echo $class_name_value ?></h2>
-                <div class="class-container-content">
-                    <div class="class-instructor-section">
-                        <h3>Instructor</h3>
-                        <img src="<?php echo $class_instructor_thumbnail_value ?>" class="instructor-headshot" alt="<?php echo $class_instructor_value ?> Headshot">
-                        <p class="instructor-name"><?php echo $class_instructor_value?></p>
+
+              <style>
+                .thumb-container {width:100%;height:150px;}
+                .thumb-container img {width:100%;height:170px!important;}
+              </style>
+            
+              <?php if( has_post_thumbnail() ): ?>
+                        <div class="thumb-container">
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                        </div>
+              <?php endif; ?>
+                
+                <div class="class-container-content" style="text-align:left;padding-top:20px;">
+                    <h3 style="color:#333;font-weight:900;"><?php echo $class_name_value ?></h3>    
+                    <div class="class-instructor-section" style="display:block;width:100%;text-align:left;">
+                        <h3>Ingredients</h3>
+                        <p style="margin-bottom:0;"><?php echo $class_instructor_thumbnail_value ?></p> 
+                        <!--
+                        <img src="< ?php echo $class_instructor_thumbnail_value ?>" class="instructor-headshot" alt="< ?php echo $class_instructor_value ?> Headshot">
+                        -->
+                        
                     </div>
+                        <!--
                         <div class="class-skill-section">
                            <h3>Skill Level</h3>
                            <p class="skill-level"><?php echo $class_skill_value?></p> 
                         </div>
-                        <div class="class-length-section">
-                          <h3>Length</h3>
+                        -->
+                        <div class="class-length-section" style="display:block;width:100%;text-align:left;margin-left:0;">
+                          <h3>Price</h3>
+                          <p class="instructor-name"><?php echo $class_instructor_value?></p>
+                          <!--    
                           <p class="length"><?php echo $class_length_value?></p>  
+                            -->
                         </div>
-                    <p class="class-description"><?php echo $class_description_value?></p>
-                    <button class="button enroll-button"><a href="">Enroll</a></button>
+                        <!--<p class="class-description"><?php echo $class_description_value?></p>-->
+                        <button class="button enroll-button" style="background-color:#00b8b8;"><a href="">Submit Order</a></button>
                  </div>   
               
           </div>
